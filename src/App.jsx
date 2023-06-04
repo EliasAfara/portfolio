5646d5704c242d3124a198d1e53a3285c3533a92
Elias Afara <afaraelias@gmail.com>
2023-06-04 22:40:43 +0200

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Elias</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
