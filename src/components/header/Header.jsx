089516e0b6a84891625eac111df9ed5f9aaf1397
Elias Afara <afaraelias@gmail.com>
2023-06-06 00:06:18 +0200

    updated header navlinks

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import "./header.css";
-import {
-  HiOutlineHome,
-  HiOutlineUser,
-  HiOutlineBadgeCheck,
-  HiOutlineClipboardList,
-  HiOutlinePhotograph,
-  HiOutlineMail,
-  HiX,
-  HiOutlineMenu,
-} from "react-icons/hi";
+import * as Icons from "react-icons/hi";
+
+import config from "../../config";
+
+const DynamicHiIcon = ({ name }) => {
+  const IconComponent = Icons[name];
+  if (!IconComponent) return <Icons.HiBan className='nav__icon' />;
+  return <IconComponent className='nav__icon' />;
+};
 
 const Header = () => {
   window.addEventListener("scroll", function () {
@@ -18,105 +17,44 @@ const Header = () => {
     else header.classList.remove("scroll-header");
   });
 
+  const navLinks = config.navLinks;
+
   const [Toggle, showMenu] = useState(false);
   const [activeNav, setActiveNav] = useState("#home");
 
   return (
     <header className='header'>
-      <nav className='nav container'>
+      <nav className='nav' style={{ padding: "0px 50px" }}>
         <a href='/' className='nav__logo'>
           <span className='grey-color'> &lt;</span>
           <span className='logo-name'>Elias</span>
           <span className='grey-color'>/&gt;</span>
         </a>
-        {/* <span className='nav__logo'>Elias Portfolio</span> */}
+
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className='nav__list grid'>
-            <li className='nav__item'>
-              <a
-                href='#home'
-                onClick={() => setActiveNav("#home")}
-                className={
-                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
-                }
-              >
-                <HiOutlineHome className='nav__icon' />
-                Home
-              </a>
-            </li>
-            <li className='nav__item'>
-              <a
-                href='#about'
-                onClick={() => setActiveNav("#about")}
-                className={
-                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
-                }
-              >
-                <HiOutlineUser className='nav__icon' />
-                About
-              </a>
-            </li>
-            <li className='nav__item'>
-              <a
-                href='#skills'
-                onClick={() => setActiveNav("#skills")}
-                className={
-                  activeNav === "#skills"
-                    ? "nav__link active-link"
-                    : "nav__link"
-                }
-              >
-                <HiOutlineBadgeCheck className='nav__icon' />
-                Skills
-              </a>
-            </li>
-            {/* <li className='nav__item'>
-              <a
-                href='#services'
-                onClick={() => setActiveNav("#services")}
-                className={
-                  activeNav === "#services"
-                    ? "nav__link active-link"
-                    : "nav__link"
-                }
-              >
-                <HiOutlineClipboardList className='nav__icon' />
-                Services
-              </a>
-            </li> */}
-            <li className='nav__item'>
-              <a
-                href='#portfolio'
-                onClick={() => setActiveNav("#portfolio")}
-                className={
-                  activeNav === "#portfolio"
-                    ? "nav__link active-link"
-                    : "nav__link"
-                }
-              >
-                <HiOutlinePhotograph className='nav__icon' />
-                Projects
-              </a>
-            </li>
-            <li className='nav__item'>
-              <a
-                href='#contact'
-                onClick={() => setActiveNav("#contact")}
-                className={
-                  activeNav === "#contact"
-                    ? "nav__link active-link"
-                    : "nav__link"
-                }
-              >
-                <HiOutlineMail className='nav__icon' />
-                Contact
-              </a>
-            </li>
+            {navLinks.map(({ id, name, url, icon }, index) => {
+              return (
+                <li className='nav__item' key={index}>
+                  <a
+                    href={url}
+                    onClick={() => setActiveNav(`${id}`)}
+                    className={
+                      activeNav === id ? "nav__link active-link" : "nav__link"
+                    }
+                  >
+                    {/* <HiOutlineHome className='nav__icon' /> */}
+                    <DynamicHiIcon name={icon} />
+                    {name}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
-          <HiX className='nav__close' onClick={() => showMenu(!Toggle)} />
+          <Icons.HiX className='nav__close' onClick={() => showMenu(!Toggle)} />
         </div>
         <div className='nav__toggle' onClick={() => showMenu(!Toggle)}>
-          <HiOutlineMenu />
+          <Icons.HiOutlineMenu />
         </div>
       </nav>
     </header>
--- src/config.js
+++ src/config.js
@@ -14,7 +14,46 @@ const config = {
       url: "https://www.linkedin.com/in/eliasafara",
     },
   ],
-  navLinks: [],
+
+  navLinks: [
+    {
+      id: "#about",
+      name: "About",
+      url: "/#about",
+      icon: "HiOutlineUser",
+    },
+    {
+      id: "#experience",
+      name: "Experience",
+      url: "/#experience",
+      icon: "HiOutlineBriefcase",
+    },
+    {
+      id: "#education",
+      name: "Education",
+      url: "/#education",
+      icon: "HiOutlineAcademicCap",
+    },
+    {
+      id: "#skills",
+      name: "Skills",
+      url: "/#skills",
+      icon: "HiOutlineBadgeCheck",
+    },
+    {
+      id: "#projects",
+      name: "Projects",
+      url: "/#projects",
+      icon: "HiOutlinePhotograph",
+    },
+
+    {
+      id: "#contact",
+      name: "Contact",
+      url: "/#contact",
+      icon: "HiOutlineMail",
+    },
+  ],
 };
 
 export default config;
