e69cb47517235ec08b6ad9610743668b32cdda23
Elias Afara <afaraelias@gmail.com>
2023-06-05 12:09:08 +0200

    feat: created the about section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  HiOutlineDesktopComputer,
+  HiOutlineTerminal,
+  HiOutlineSparkles,
+} from "react-icons/hi";
+
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+      <div className='about__box'>
+        <HiOutlineDesktopComputer className='about__icon' />
+        <h3 className='about__title'>Experience</h3>
+        <span className='about__subtitle'>3 Years</span>
+      </div>
+      <div className='about__box'>
+        <HiOutlineTerminal className='about__icon' />
+        <h3 className='about__title'>Completed</h3>
+        <span className='about__subtitle'>35+ Projects</span>
+      </div>
+      <div className='about__box'>
+        <HiOutlineSparkles className='about__icon' />
+        <h3 className='about__title'>Support</h3>
+        <span className='about__subtitle'>Online 24/7</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
