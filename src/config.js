7664546bd65aab24f8b23d317ba8f9c8cc7fa612
Elias Afara <afaraelias@gmail.com>
2023-06-07 23:58:58 +0200

    modified config

--- src/config.js
+++ src/config.js
@@ -1,59 +1,78 @@
-const config = {
+const contactInfo = {
   email: "afaraelias@gmail.com",
-  socialMedia: [
-    {
-      name: "GitHub",
-      url: "https://github.com/eliasafara",
-    },
-    {
-      name: "Twitter",
-      url: "https://twitter.com/thegrindev",
-    },
-    {
-      name: "Linkedin",
-      url: "https://www.linkedin.com/in/eliasafara",
-    },
-  ],
-
-  navLinks: [
-    {
-      id: "#about",
-      name: "About",
-      url: "/#about",
-      icon: "HiOutlineUser",
-    },
-    {
-      id: "#experience",
-      name: "Experience",
-      url: "/#experience",
-      icon: "HiOutlineBriefcase",
-    },
-    {
-      id: "#education",
-      name: "Education",
-      url: "/#education",
-      icon: "HiOutlineAcademicCap",
-    },
-    {
-      id: "#skills",
-      name: "Skills",
-      url: "/#skills",
-      icon: "HiOutlineBadgeCheck",
-    },
-    {
-      id: "#projects",
-      name: "Projects",
-      url: "/#projects",
-      icon: "HiOutlinePhotograph",
-    },
-
-    {
-      id: "#contact",
-      name: "Contact",
-      url: "/#contact",
-      icon: "HiOutlineMail",
-    },
-  ],
 };
 
-export default config;
+const education = [
+  {
+    schoolName: "EPITA - ECOLE D'INGÉNIEURS INFORMATIQUE PARIS",
+    degree: "MSc in CS - Software Engineering",
+    date: "September 2021 - March 2023",
+    location: "Le Kremlin-Bicêtre, France",
+  },
+  {
+    schoolName: "Phoenicia University",
+    degree: "Bachelor of Science in Computer Science",
+    date: "January 2017 - March 2021",
+    location: "Lebanon",
+  },
+];
+
+// Note: currently trying to using markdown files for the work experience
+const workExperience = [];
+
+const socialMedia = [
+  {
+    name: "GitHub",
+    url: "https://github.com/eliasafara",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/thegrindev",
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/eliasafara",
+  },
+];
+
+const navLinks = [
+  {
+    id: "#about",
+    name: "About",
+    url: "/#about",
+    icon: "HiOutlineUser",
+  },
+  {
+    id: "#experience",
+    name: "Experience",
+    url: "/#experience",
+    icon: "HiOutlineBriefcase",
+  },
+  {
+    id: "#education",
+    name: "Education",
+    url: "/#education",
+    icon: "HiOutlineAcademicCap",
+  },
+  {
+    id: "#skills",
+    name: "Skills",
+    url: "/#skills",
+    icon: "HiOutlineBadgeCheck",
+  },
+  {
+    id: "#projects",
+    name: "Projects",
+    url: "/#projects",
+    icon: "HiOutlinePhotograph",
+  },
+
+  {
+    id: "#contact",
+    name: "Contact",
+    url: "/#contact",
+    icon: "HiOutlineMail",
+  },
+];
+
+export { contactInfo, socialMedia, navLinks };
