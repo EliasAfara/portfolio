1f7f04d7bddb2b7ac97fcadd90bda83f080a43f1
Elias Afara <afaraelias@gmail.com>
2023-06-13 00:15:11 +0200

    updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,34 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 import Email from "./components/Email";
+import Socials from "./components/Socials";
+// import ScrollUp from "./components/scrollup/ScrollUp";
+import Footer from "./components/footer/Footer";
+import Jobs from "./components/jobs/Jobs";
+
+//
+import Education from "./components/education/Education";
 
 function App() {
   return (
     <>
       <Header />
+      <Socials />
       <Email />
       <main className='main'>
         <Home />
         <About />
-        {/*<Skills />
-        <Qualification />
-        <Portfolio />
+        {/* <Experience />*/}
+        <Education />
+        <Skills />
+        {/* <Projects />
         <Contact /> */}
+        <Jobs />
       </main>
-      {/* <Footer />
-      <ScrollUp /> */}
+      <Footer />
+      {/* <ScrollUp /> */}
     </>
   );
 }
