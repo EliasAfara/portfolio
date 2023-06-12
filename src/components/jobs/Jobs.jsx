4d3b2efb2e54b0de27c595004d169051604cdb58
Elias Afara <afaraelias@gmail.com>
2023-06-13 00:18:29 +0200

    Created demo Jobs section

---
+++ src/components/jobs/Jobs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import styled from "styled-components";
+
+const Jobs = () => {
+  const data = () => {};
+  const jobsData = data;
+
+  return <div>Jobs</div>;
+};
+
+export default Jobs;
