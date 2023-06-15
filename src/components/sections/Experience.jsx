d9dc9ca6369b55e01673b3c15f16f0d2d94d378d
Elias Afara <afaraelias@gmail.com>
2023-06-15 23:07:08 +0200

    relocated Experience jsx file to sections dir

---
+++ src/components/sections/Experience.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import styled from "styled-components";
+
+const Experience = () => {
+  const data = () => {};
+  const jobsData = data;
+
+  return <div id='experience'>Experience</div>;
+};
+
+export default Experience;
