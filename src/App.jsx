acc7c0b3ac10d37ebcb8cda34765a01d24fd3067
Elias Afara <afaraelias@gmail.com>
2023-06-15 16:53:45 +0200

    added styled component global style

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,44 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Email from "./components/Email";
 import Socials from "./components/Socials";
-// import ScrollUp from "./components/scrollup/ScrollUp";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer";
 import Jobs from "./components/jobs/Jobs";
 
-//
+// import ScrollUp from "./components/scrollup/ScrollUp";
+
 import Education from "./components/education/Education";
+
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "@/styles";
+
+const StyledMainContainer = styled.main`
+  counter-reset: section;
+`;
 
 function App() {
   return (
     <>
-      <Header />
-      <Socials />
-      <Email />
-      <main className='main'>
-        <Home />
-        <About />
-        {/* <Experience />*/}
-        <Education />
-        <Skills />
-        {/* <Projects />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+
+        <Header />
+        <Socials />
+        <Email />
+
+        <StyledMainContainer className='fillHeight'>
+          <Home />
+          <About />
+          {/* <Experience />*/}
+          <Education />
+          <Skills />
+          {/* <Projects />
         <Contact /> */}
-        <Jobs />
-      </main>
-      <Footer />
-      {/* <ScrollUp /> */}
+          <Jobs />
+        </StyledMainContainer>
+
+        <Footer />
+        {/* <ScrollUp /> */}
+      </ThemeProvider>
     </>
   );
 }
