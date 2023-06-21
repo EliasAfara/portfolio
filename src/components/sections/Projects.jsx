9ba0303ac5ab927d85e1699d1534a6942fac15eb
Elias Afara <afaraelias@gmail.com>
2023-06-21 23:48:01 +0200

    removed responsive styling for projects display

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -123,16 +123,8 @@ const StyledProject = styled.li`
   grid-template-columns: repeat(12, 1fr);
   align-items: center;
 
-  @media (max-width: 768px) {
-    ${({ theme }) => theme.mixins.boxShadow};
-  }
-
   &:not(:last-of-type) {
     margin-bottom: 100px;
-
-    @media (max-width: 768px) {
-      margin-bottom: 70px;
-    }
 
     @media (max-width: 480px) {
       margin-bottom: 30px;
@@ -147,20 +139,10 @@ const StyledProject = styled.li`
       @media (max-width: 1080px) {
         grid-column: 5 / -1;
       }
-      @media (max-width: 768px) {
-        grid-column: 1 / -1;
-        padding: 40px 40px 30px;
-        text-align: left;
-      }
-      @media (max-width: 480px) {
-        padding: 25px 25px 20px;
-      }
     }
 
     .project-title {
-      @media (min-width: 768px) {
-        margin-left: auto;
-      }
+      margin-left: auto;
     }
 
     .project-tech-list {
@@ -172,22 +154,12 @@ const StyledProject = styled.li`
 
       li {
         margin: 0 0 5px 20px;
-
-        @media (max-width: 768px) {
-          margin: 0 10px 5px 0;
-        }
       }
     }
     .project-links {
       justify-content: flex-end;
       margin-left: 0;
       margin-right: -10px;
-
-      @media (max-width: 768px) {
-        justify-content: flex-start;
-        margin-left: -10px;
-        margin-right: 0;
-      }
     }
     .project-image {
       grid-column: 1 / 8;
@@ -202,24 +174,6 @@ const StyledProject = styled.li`
     position: relative;
     grid-column: 1 / 7;
     grid-row: 1 / -1;
-
-    @media (max-width: 1080px) {
-      grid-column: 1 / 9;
-    }
-
-    @media (max-width: 768px) {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      height: 100%;
-      grid-column: 1 / -1;
-      padding: 40px 40px 30px;
-      z-index: 5;
-    }
-
-    @media (max-width: 480px) {
-      padding: 30px 25px 20px;
-    }
   }
 
   .project-overline {
@@ -234,33 +188,10 @@ const StyledProject = styled.li`
     width: 60%;
     color: var(--lightest-slate);
     font-size: clamp(24px, 5vw, 28px);
-
-    @media (min-width: 768px) {
-      margin-bottom: 20px;
-    }
-
-    @media (max-width: 768px) {
-      color: var(--white);
-
-      a {
-        position: static;
-
-        &:before {
-          content: "";
-          display: block;
-          position: absolute;
-          z-index: 0;
-          width: 100%;
-          height: 100%;
-          top: 0;
-          left: 0;
-        }
-      }
-    }
   }
 
   .project-description {
-    ${({ theme }) => theme.mixins.boxShadow};
+    // ${({ theme }) => theme.mixins.boxShadow};
     position: relative;
     z-index: 2;
     padding: 25px;
@@ -268,16 +199,6 @@ const StyledProject = styled.li`
     background-color: var(--light-navy);
     color: var(--light-slate);
     font-size: var(--fz-lg);
-
-    @media (max-width: 768px) {
-      padding: 20px 0;
-      background-color: transparent;
-      box-shadow: none;
-
-      &:hover {
-        box-shadow: none;
-      }
-    }
 
     a {
       ${({ theme }) => theme.mixins.inlineLink};
@@ -305,15 +226,6 @@ const StyledProject = styled.li`
       font-size: var(--fz-xs);
       white-space: nowrap;
     }
-
-    @media (max-width: 768px) {
-      margin: 10px 0;
-
-      li {
-        margin: 0 10px 5px 0;
-        color: var(--lightest-slate);
-      }
-    }
   }
 
   .project-links {
@@ -327,14 +239,6 @@ const StyledProject = styled.li`
     a {
       ${({ theme }) => theme.mixins.flexCenter};
       padding: 10px;
-
-      //   &.external {
-      //     svg {
-      //       width: 22px;
-      //       height: 22px;
-      //       margin-top: -4px;
-      //     }
-      //   }
 
       svg {
         width: 20px;
@@ -355,12 +259,6 @@ const StyledProject = styled.li`
     position: relative;
     z-index: 1;
 
-    @media (max-width: 768px) {
-      grid-column: 1 / -1;
-      height: 100%;
-      opacity: 0.25;
-    }
-
     a {
       width: 100%;
       height: 100%;
@@ -368,45 +266,23 @@ const StyledProject = styled.li`
       border-radius: var(--border-radius);
       vertical-align: middle;
 
-      //   &:hover,
-      //   &:focus {
-      //     background: transparent;
-      //     outline: 0;
+      &:hover,
+      &:focus {
+        background: transparent;
+        outline: 0;
 
-      //     &:before,
-      //     .img {
-      //       background: transparent;
-      //       filter: none;
-      //     }
-      //   }
-
-      //   &:before {
-      //     content: "";
-      //     position: absolute;
-      //     width: 100%;
-      //     height: 100%;
-      //     top: 0;
-      //     left: 0;
-      //     right: 0;
-      //     bottom: 0;
-      //     z-index: 3;
-      //     transition: var(--transition);
-      //     background-color: var(--navy);
-      //     mix-blend-mode: screen;
-      //   }
+        &:before,
+        .img {
+          background: transparent;
+          filter: none;
+        }
+      }
     }
 
     .img {
       border-radius: var(--border-radius);
       //   mix-blend-mode: multiply;
       //   filter: grayscale(100%) contrast(1) brightness(90%);
-
-      @media (max-width: 768px) {
-        object-fit: cover;
-        width: auto;
-        height: 100%;
-        filter: grayscale(100%) contrast(1) brightness(50%);
-      }
     }
   }
 `;
