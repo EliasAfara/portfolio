60a152472a7891e7d6976354c5520f9c1a79ad36
Elias Afara <afaraelias@gmail.com>
2023-06-24 08:52:26 +0200

    feat: created page not found - 404

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledMainContainer = styled.main`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+`;
+const StyledTitle = styled.h1`
+  color: var(--moonstone-blue);
+  font-family: var(--font-mono);
+  font-size: clamp(100px, 25vw, 200px);
+  line-height: 1;
+`;
+const StyledSubtitle = styled.h2`
+  font-size: clamp(30px, 5vw, 50px);
+  font-weight: 400;
+`;
+const StyledHomeButton = styled(Link)`
+  ${({ theme }) => theme.mixins.bigButton};
+  margin-top: 40px;
+`;
+
+const PageNotFound = () => {
+  return (
+    <StyledMainContainer className='fillHeight'>
+      <StyledTitle>404</StyledTitle>
+      <StyledSubtitle>Page Not Found</StyledSubtitle>
+      <StyledHomeButton to='/'>Go Home</StyledHomeButton>
+    </StyledMainContainer>
+  );
+};
+
+export default PageNotFound;
