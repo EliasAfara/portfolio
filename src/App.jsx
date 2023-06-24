da070041d4af00e4db063f462d888ed3641c3875
Elias Afara <afaraelias@gmail.com>
2023-06-24 09:26:47 +0200

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,46 @@ import Email from "./components/Email";
 import Socials from "./components/Socials";
 import Footer from "./components/Footer";
 
+// pages
+import PageNotFound from "./pages/PageNotFound";
+
 // import ScrollUp from "./components/scrollup/ScrollUp";
 
 import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyle, theme } from "@/styles";
+import { Routes, Route, Outlet } from "react-router-dom";
 
-const StyledMainContainer = styled.main`
-  counter-reset: section;
-`;
+const MainRoute = () => {
+  return (
+    <StyledMainContainer className='fillHeight'>
+      <Home />
+      <About />
+      <Education />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contact />
+    </StyledMainContainer>
+  );
+};
+
+const Layout = () => {
+  return (
+    <>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <Header />
+      <Socials />
+      <Email />
+
+      {/* An <Outlet> renders whatever child route is currently active */}
+      <Outlet />
+
+      <Footer />
+      {/* <ScrollUp /> */}
+    </>
+  );
+};
 
 function App() {
   return (
@@ -30,25 +62,20 @@ function App() {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
 
-        <Header />
-        <Socials />
-        <Email />
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<MainRoute />} />
 
-        <StyledMainContainer className='fillHeight'>
-          <Home />
-          <About />
-          <Education />
-          <Experience />
-          <Skills />
-          <Projects />
-          <Contact />
-        </StyledMainContainer>
-
-        <Footer />
-        {/* <ScrollUp /> */}
+            <Route path='*' element={<PageNotFound />} />
+          </Route>
+        </Routes>
       </ThemeProvider>
     </>
   );
 }
+
+const StyledMainContainer = styled.main`
+  counter-reset: section;
+`;
 
 export default App;
